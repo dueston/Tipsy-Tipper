466e242241ddc99e64138a76923f181ffeee2021
William Daugherty <raidantlabs@gmail.com>
2020-09-13 02:41:56 -0700

    Initial Commit

---
+++ Tipsy Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy Tipper
+//
+//  Created by William Daugherty on 9/13/20.
+//  Copyright © 2020 William Daugherty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
